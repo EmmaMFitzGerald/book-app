7f04404355aea1f62f17ae7f3dc4932f0bfc8b93
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2020-02-18 18:08:49 -0500

    added getBooks method

--- Books/src/app/book/book.component.ts
+++ Books/src/app/book/book.component.ts
@@ -1,13 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { BookService } from './book.service';
 
 @Component({
   selector: 'app-book',
   templateUrl: './book.component.html',
-  styleUrls: ['./book.component.scss']
+  styleUrls: ['./book.component.scss'],
+  providers: [BookService]
 })
-export class BookComponent implements OnInit {
 
-  constructor() { }
+export class BookComponent implements OnInit {
+  private books;
+  private activeTasks;
+
+  constructor(private bookService: BookService) { }
+
+  getBooks(){
+    return this.bookService.get().then(books => {
+      this.books = books;
+      this.activeTasks = this.books.filter(book => !book.read).length;
+    })
+  }
 
   ngOnInit(): void {
   }
