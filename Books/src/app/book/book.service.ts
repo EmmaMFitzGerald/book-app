1147403588e66422b7439cda3be32e51a94044cd
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2020-02-18 18:03:43 -0500

    added dummy data for books and a get method to get books

--- Books/src/app/book/book.service.ts
+++ Books/src/app/book/book.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
 
+const BOOKS = [
+   {title: "Golden in Death", author: "J.D. Robb", read: true},
+   {title: "Crooked River", author: "Lincoln Child", read: true},
+   {title: "American Dirt", author: "J Cummins", read: false},
+   {title: "Where the Crawdads Sing", author: "Delia Owens", read: false}
+]
+
 @Injectable({
   providedIn: 'root'
 })
+
 export class BookService {
 
   constructor() { }
+
+  get() {
+    return new Promise(resolve => resolve(BOOKS));
+  }
+  
 }
